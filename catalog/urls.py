d60393e0084e487de3e2642798a4f16b2793d58e
yeezy_na_izi <howone1sky@gmail.com>
2022-03-13 18:36:40 +0300

    pk now only int

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from catalog import views
 
 urlpatterns = [
-    path('<pk>/', views.item_detail, name='item_detail'),
+    path('<int:pk>/', views.item_detail, name='item_detail'),
     path('', views.item_list, name='all_items'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('users/<pk>/', views.user_detail, name='detail_users'),
+    path('users/<int:pk>/', views.user_detail, name='detail_users'),
     path('users/', views.user_list, name='all_users'),
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
