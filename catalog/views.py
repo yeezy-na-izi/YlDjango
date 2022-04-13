f7d13cb7dfed77c27f6b84a37c0b8866ddc57829
yeezy_na_izi <howone1sky@gmail.com>
2022-04-13 21:43:47 +0300

    really hotfix

--- catalog/views.py
+++ catalog/views.py
@@ -21,19 +21,20 @@ class ItemDetail(View):
     items = Item.objects.published_item_and_tags()
 
     def get(self, request, pk):
-        star_dict = Rating.choices
+        stars_variants = Rating.choices
+
         item = get_object_or_404(self.items, pk=pk)
-        stars = Rating.objects.filter(item=item, star__in=[1, 2, 3, 4, 5]).aggregate(Avg('star'), Count('star'))
+        stars = item.item_stars.exclude(star=0).aggregate(Avg('star'), Count('star'))
+
+        user_star = 0
         if request.user.is_authenticated:
-            try:
-                user_star = Rating.objects.get(item=item, user=request.user).star
-            except Rating.DoesNotExist:
-                user_star = 0
-        else:
-            user_star = 0
+            user_rate = Rating.objects.filter(item=item, user=request.user).first()
+            if user_rate:
+                user_star = user_rate.star
+
         context = {
             'item': item,
-            'star_dict': star_dict,
+            'stars_variants': stars_variants,
             'stars': stars,
             'user_star': user_star
         }
@@ -42,16 +43,14 @@ class ItemDetail(View):
 
     def post(self, request, pk):
         item = get_object_or_404(self.items, pk=pk)
-        if request.POST['rate'] in ['0', '1', '2', '3', '4', '5'] and request.user.is_authenticated:
-            obj, created = Rating.objects.get_or_create(
-                user=request.user,
-                item=item,
-                defaults={
-                    'item': item,
-                    'user': request.user,
-                }
-            )
 
-            obj.star = int(request.POST['rate'])
-            obj.save()
+        if 'rate' in request.POST and request.POST['rate'].isdigit():
+            rate = int(request.POST['rate'])
+
+            if rate in Rating.choices and request.user.is_authenticated:
+                Rating.objects.update_or_create(
+                    item=item,
+                    user=request.user,
+                    defaults={'star': rate}
+                )
         return redirect('item_detail', pk=pk)
