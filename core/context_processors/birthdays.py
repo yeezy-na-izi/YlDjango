37f172039c52b7d90f52f6f562f3681ab39b9fa9
Powik174 <olegzhuchkov2005@gmail.com>
2022-04-30 18:56:20 +0700

    fixed birthday filter in context processors

--- core/context_processors/birthdays.py
+++ core/context_processors/birthdays.py
@@ -4,7 +4,8 @@ from django.utils.timezone import now
 
 def get_users_birthdays(request):
     return {'users_birthdays':
-            Profile.objects.filter(birthday=now().date())
+            Profile.objects.filter(birthday__month=now().month,
+                                   birthday__day=now().day)
             .only('user__first_name', 'user__last_name')
             .order_by('user__first_name', 'user__last_name')
             }
