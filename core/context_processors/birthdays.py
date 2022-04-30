6fd0763183d76e4f7ca4a9686170d0665d7c04e9
yeezy_na_izi <howone1sky@gmail.com>
2022-04-30 15:00:51 +0300

    flake8 and add birthdays section

--- core/context_processors/birthdays.py
+++ core/context_processors/birthdays.py
@@ -3,9 +3,10 @@ from django.utils.timezone import now
 
 
 def get_users_birthdays(request):
-    return {'users_birthdays':
-            Profile.objects.filter(birthday__month=now().month,
-                                   birthday__day=now().day)
-            .only('user__first_name', 'user__last_name')
-            .order_by('user__first_name', 'user__last_name')
-            }
+    return {
+        'users_birthdays':
+            Profile.objects.filter(
+                birthday__month=now().month,
+                birthday__day=now().day
+            ).only('user__first_name', 'user__last_name').order_by('user__first_name', 'user__last_name')
+    }
